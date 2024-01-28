e3870bca5a47db03b5164dcefa9fe0876b6f0f6e
Dutt Patel <127007807+Dutt0408@users.noreply.github.com>
2024-01-27 23:35:13 -0500

    Commit kebverve

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@
 // import './App.css';
 // import './index.css'
 
-// import Navbar from './navbar'
-import { ReactFinalFormDemo } from "./ReactFinalFormDemo";
+import Navbar from './navbar'
+// import { ReactFinalFormDemo } from "./ReactFinalFormDemo";
 
 
 function App() {
   return (
    <>
 
-    {/* <Navbar/> */} 
+    <Navbar/> 
     
-        <ReactFinalFormDemo/>
+        {/* <ReactFinalFormDemo/> */}
 
    </>
   );
